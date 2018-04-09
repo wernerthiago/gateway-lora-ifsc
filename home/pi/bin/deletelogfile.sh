78bf47463339f75264b5ff1af4678cc21794bcf7
arctouch-thiago.werner <thiago.werner@arctouch.com>
2018-04-08 23:38:41 -0300

    Adicionado log e configuracao do gateway

---
+++ home/pi/bin/deletelogfile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+	curl -F "operation=upload" -F "file=@/home/pi/risinghf/packet_forwarder/logfile.log" https://iot.sj.ifsc.edu.br/~thiago.werner/log.php
+	cd /home/pi/risinghf/packet_forwarder
+	file=logfile.log
+	minimumsize=90000
+	actualsize=$(wc -c <"$file")
+	if [ $actualsize -ge $minimumsize ]; then
+		cat /dev/null > logfile.log
+	fi
