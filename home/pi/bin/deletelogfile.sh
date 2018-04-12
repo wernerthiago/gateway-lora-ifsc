8a63df9983f746ebc0ed0b16b703e77108f24a63
Thiago Werner <wernerthiago@gmail.com>
2018-04-12 19:42:25 -0300

    Sincronizando diretórios

--- home/pi/bin/deletelogfile.sh
+++ home/pi/bin/deletelogfile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-	curl -F "operation=upload" -F "file=@/home/pi/risinghf/packet_forwarder/logfile.log" https://iot.sj.ifsc.edu.br/~thiago.werner/log.php
+	curl -F "operation=upload" -F "file=@/home/pi/risinghf/packet_forwarder/lora_pkt_fwd/logfile.log" https://iot.sj.ifsc.edu.br/~thiago.werner/log.php
 	cd /home/pi/risinghf/packet_forwarder
 	file=logfile.log
 	minimumsize=90000
