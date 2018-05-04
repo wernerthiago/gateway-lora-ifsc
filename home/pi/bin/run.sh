9ac7b7dd0069152f9600115716277968e48bce90
Thiago Werner <wernerthiago@gmail.com>
2018-05-03 22:12:16 -0300

    Update run.sh
    Adicionando loop de execução do script.

--- home/pi/bin/run.sh
+++ home/pi/bin/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+while true; do
 	/home/pi/risinghf/lora_gateway/reset_lgw.sh start 7
 	cd /home/pi/risinghf/packet_forwarder/lora_pkt_fwd
-	./lora_pkt_fwd >> logfile.log &
+	./lora_pkt_fwd >> /home/pi/risinghf/packet_forwarder/logfile.log &
+	wait $!
+	sleep 1
+done
+exit
